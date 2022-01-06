7c04b2f72958f92a4c2d7d75cfea331b983801e1
marco raphael dantas campos <marco-raphael>
2022-01-06 13:27:28 -0300

    feito o map pra renderizar pokemons na pag da pokedex e botao de voltar a lista

--- pokedex/src/Pages/PokedexPage/PokedexPage.js
+++ pokedex/src/Pages/PokedexPage/PokedexPage.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { useContext } from "react";
+import { goToDetail, goToHome } from "../../Router/coordinator";
+import GlobalContext from "../../contexts/GlobalContext";
+import { useHistory } from 'react-router-dom';
+import PokemonCard from "../../components/PokemonCard/PokemonCard";
+import styled from "styled-components";
+
+
+const Div = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
 
 function PokedexPage() {
+  const history = useHistory()
+  const {states, setters, requests} = useContext(GlobalContext)
+
+  console.log(states)
+
+  const pokedexList = states.pokedex.map((pokemon)=>{
+    return(
+      <PokemonCard key={pokemon.url}
+      name={pokemon.name}
+      order={pokemon.order}
+      url={pokemon.url}
+      add={()=> requests.addPokemon(pokemon)}
+      det={()=> goToDetail(history, pokemon.name)}
+      
+    />
+    )
+  })
+
   return (
-    <div >
+    <Div >
+     
       PokedexPage
-    </div>
+      {pokedexList === [] ? <p>Loading</p> : pokedexList}
+      {/* {pokedexList} */}
+      <button onClick={() => goToHome(history)}>voltar para lista de pokemons</button>
+    </Div>
   );
 }
 
