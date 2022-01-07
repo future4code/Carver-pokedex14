6e1c4816f7259a196a4048d7fe91c0ae2ccc1695
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-01-07 19:40:48 -0400

    Começando a tela de detalhes.

--- pokedex/src/Pages/DetailPage/DetailPage.js
+++ pokedex/src/Pages/DetailPage/DetailPage.js
@@ -1,20 +1,35 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from 'react'
 import URL from '../../constants/url'
-import axios from "axios";
-import { useHistory, useParams } from "react-router";
-import styled from "styled-components";
-import GlobalContext from "../../contexts/GlobalContext";
-
+import axios from 'axios'
+import { useHistory, useParams } from 'react-router'
+import styled from 'styled-components'
+import GlobalContext from '../../contexts/GlobalContext'
 
 function DetailPage() {
+  const [pokemonDetails, setPokemonDetails] = useState([])
 
+  useEffect(() => {
+    getPokemonDetails()
+  }, [])
 
-  return (
-    <div >
-      
-      
-    </div>
-  );
+  const getPokemonDetails = () => {
+    const list = []
+    const organizerList = list.sort()
+    for (let i = 1; i <= 20; i++) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then(response => {
+          list.push(response.data)
+          if (organizerList.length === 20) {
+            setPokemonDetails(organizerList)
+          }
+          console.log(list)
+        })
+        .catch(error => console.log(error.message))
+    }
+  }
+
+  return <div></div>
 }
 
-export default DetailPage;
+export default DetailPage
